27bed741f6b098f9acecdc9628a4501627cf8a6e
PEREIRA Deborah <Deborah.Pereira@bordeaux-inp.fr>
2022-01-17 15:09:55 +0100

    Import unused

--- datacleaning/src/main/java/TweetMapper.java
+++ datacleaning/src/main/java/TweetMapper.java
@@ -6,10 +6,7 @@ import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class TweetMapper extends Mapper<LongWritable, Text, LongWritable, TweetWritable> {
 
